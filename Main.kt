df71a0377f12299cd59ac78317c567b3395c3c78
l000ps <toporoleksandr@gmail.com>
2023-06-08 12:26:35 +0300

    Stage 2/6: The tasklist menu

--- Main.kt
+++ Main.kt
@@ -1,26 +1,56 @@
 package tasklist
 
+import kotlin.system.exitProcess
+
+val tasks = mutableListOf<MutableList<String>>()
+
 fun main() {
-    val tasks = mutableListOf<String>()
-
-    // Ввод заданий
-    println("Input the tasks (enter a blank line to end):")
     while (true) {
-        val task = readLine()?.trim()
-        if (task.isNullOrBlank()) {
-            break
+        println("Input an action (add, print, end):")
+        when (readln()) {
+            "add" -> add()
+            "print" -> print()
+            "end" -> {
+                println("Tasklist exiting!")
+                exitProcess(0)
+            }
+            else -> println("The input action is invalid")
         }
-        tasks.add(task)
     }
+}
 
-    // Вывод заданий с номерами
+fun add() {
+    println("Input a new task (enter a blank line to end):")
+    val task = mutableListOf<String>()
+    while (true) {
+        val userInp = readLine()?.trim()
+        if (userInp.isNullOrBlank()) {
+            if (task.size == 0) {
+                println("The task is blank")
+                return
+            }
+            break
+        } else {
+            task.add(userInp)
+        }
+    }
+    tasks.add(task)
+}
+
+fun print() {
     if (tasks.isEmpty()) {
         println("No tasks have been input")
     } else {
         for ((index, task) in tasks.withIndex()) {
             val taskNumber = index + 1
-            val indentation = if (taskNumber in 1..9) "  " else " "
-            println("$taskNumber$indentation$task")
+            for (i in 0 until task.size) {
+                if (i == 0) {
+                    println(String.format("%-3s%s", taskNumber, task[i]))
+                } else {
+                    println(String.format("   " + task[i]))
+                }
+            }
+            println()
         }
     }
 }
