15e61d0f23e2a9b241da9601a8f146aca449b13f
l000ps <toporoleksandr@gmail.com>
2023-06-12 00:14:10 +0300

    Stage 3/6: Date and time

--- Main.kt
+++ Main.kt
@@ -1,5 +1,8 @@
 package tasklist
 
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toLocalDate
+import java.time.LocalTime
 import kotlin.system.exitProcess
 
 val tasks = mutableListOf<MutableList<String>>()
@@ -20,6 +23,37 @@ fun main() {
 }
 
 fun add() {
+    var priority = ""
+    var date = ""
+    var time = ""
+    while (true) {
+        println("Input the task priority (C, H, N, L):")
+        priority = readln()
+        if (priority.lowercase() == "c" || priority.lowercase() == "h" || priority.lowercase() == "n" || priority.lowercase() == "l") {
+            break
+        }
+    }
+    while (true) {
+        println("Input the date (yyyy-mm-dd):")
+        try {
+            val inp = readln().split("-")
+            date = LocalDate.parse("${inp[0]}-${inp[1].padStart(2, '0')}-${inp[2].padStart(2, '0')}").toString()
+            break
+        } catch (e: java.lang.Exception) {
+            println("The input date is invalid")
+        }
+    }
+    while (true) {
+        println("Input the time (hh:mm):")
+        try {
+            val inp = readln().split(":")
+            time = LocalTime.parse("${inp[0].padStart(2, '0')}:${inp[1].padStart(2, '0')}").toString()
+            break
+        } catch (e:java.lang.Exception) {
+            println("The input time is invalid")
+        }
+
+    }
     println("Input a new task (enter a blank line to end):")
     val task = mutableListOf<String>()
     while (true) {
@@ -34,6 +68,7 @@ fun add() {
             task.add(userInp)
         }
     }
+    task.add(0, "$date $time ${priority.uppercase()}")
     tasks.add(task)
 }
 
